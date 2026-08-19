c2eddc6980f7e61371520598d175f44a059a062b
vishalkumar182 <vishal8049kumar@gmail.com>
2026-08-19 19:06:52 +0530

    first programw

--- hello.py
+++ hello.py
@@ -1,2 +1,29 @@
-print("i am learning python")
-print('hi vishal')
+print('i am learning python')
+print("Vishal's text") #double quotes can be used to include single quotes in the string
+print('''hi vishal how are you''')  ## single triple quotes can be used for multi-line strings
+print("""triple double quotes can be used for multi-line strings""")
+
+
+print(10) # printing integer value output 10
+print("10")# printing string value output 10
+print(10+20) # addition of two numbers output 30
+print("10"+"20") # concatenation of two strings output 1020
+print("10"+"20"+"30") # concatenation of three strings output 102030
+print(10-3) # subtraction of two numbers output 7
+print(10*3) # multiplication of two numbers output 30
+print(10/3) # division of two numbers output 3.3333333333333335
+print(10//3) # floor division of two numbers output 3
+print(10%3) # modulus of two numbers output 1 
+print(10**3) # exponentiation of two numbers output 1000
+print(10>3) # greater than operator output True
+print(10<3) # less than operator output False
+print(10==3) # equal to operator output False
+print(10!=3) # not equal to operator output True
+print(10>=3) # greater than or equal to operator output True
+print(10<=3) # less than or equal to operator output False
+print(10>3 and 10<20) # logical and operator output True
+print(10>3 or 10<20) # logical or operator output True
+print(not(10>3)) # logical not operator output False
+print(10>3 and 10<20 or 10==3) # logical operators output True
+print(10>3 and 10<20 or 10==3 and 10!=3) # logical operators output True
+
