5d856749d3c93cdaa0e8f16d18f92566676fbaff
vishalkumar182 <vishal8049kumar@gmail.com>
2026-08-24 17:18:28 +0530

    operators   overview

---
+++ pyhon_operators/assignment_operator.py
@@ -0,0 +1,47 @@
+# Assignment Operators in Python
+# Assignment operators are used to assign values to variables.   
+
+x = 5  # assigns the value 5 to the variable x
+print(x)  # prints the value of x
+
+x+= 3  # adds 3 to the current value of x and assigns the result back to x
+print(x)  # prints the updated value of x
+
+x-= 2  # equivalent to x = x - 2, subtracts 2 from the current value of x and assigns the result back to x
+print(x)  # prints the updated value of x
+
+x*= 4  # equivalent to x = x * 4, multiplies the current value of x by 4 and assigns the result back to x
+print(x)  # prints the updated value of x
+
+x/= 2  # equivalent to x = x / 2, divides the current value of x by 2 and assigns the result back to x
+print(x)  # prints the updated value of x
+
+x%= 3  # equivalent to x = x % 3, calculates the modulus of the current value of x by 3 and assigns the result back to x
+print(x)  # prints the updated value of x
+
+x**= 2  # equivalent to x = x ** 2, raises the current value of x to the power of 2 and assigns the result back to x
+print(x)  # prints the updated value of x
+
+x//= 3  # equivalent to x = x // 3, performs floor division of the current value of x by 3 and assigns the result back to x
+print(x)  # prints the updated value of x
+
+x= 5  # reassigns the value 5 to the variable x
+print(x)  # prints the reassigned value of x
+
+x&= 3  # equivalent to x = x & 3, performs a bitwise AND operation between the current value of x and 3 and assigns the result back to x
+print(x)  # prints the updated value of x   
+
+x|= 2  # equivalent to x = x | 2, performs a bitwise OR operation between the current value of x and 2 and assigns the result back to x
+print(x)  # prints the updated value of x   
+
+x^= 1  # equivalent to x = x ^ 1, performs a bitwise XOR operation between the current value of x and 1 and assigns the result back to x
+print(x)  # prints the updated value of x
+
+x<<= 1  # equivalent to x = x << 1, performs a left shift operation on the current value of x by 1 and assigns the result back to x
+print(x)  # prints the updated value of x
+
+x>>= 1  # equivalent to x = x >> 1, performs a right shift operation on the current value of x by 1 and assigns the result back to x
+print(x)  # prints the updated value of x
+
+x= 5  # reassigns the value 5 to the variable x
+print(x)  # prints the reassigned value of x
---
+++ pyhon_operators/operator_basic.py
@@ -0,0 +1,9 @@
+# operators in python:
+# operators perform operations on variables and values. In python, we have the following types of operators:
+# 1. Arithmetic operators
+# 2. Assignment operators
+# 3. Comparison operators
+# 4. Logical operators
+# 5. Identity operators
+# 6. Membership operators
+# 7. Bitwise operators
