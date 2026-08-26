a031386f88e995e2640113469a95a63051f8d456
vishalkumar182 <vishal8049kumar@gmail.com>
2026-08-26 20:26:28 +0530

    input and output in python

--- input_opt_stm.py
+++ input_opt_stm.py
@@ -17,3 +17,17 @@ print(type(age))  # this will show that the type of 'age' is <class 'str'>
 converted_age = int(age)  # converting the string input to an integer
 print(converted_age)
 print(type(converted_age))  # this will show that the type of 'converted_age' is <class 'int'>
+
+# example of taking float input
+height = input("Enter your height in meters: ")  # takes input from the user and assigns it to the variable 'height'
+converted_height = float(height)  # converting the string input to a float
+print(converted_height)
+print(type(converted_height))  # this will show that the type of 'converted_height' is <class 'float'>
+
+# real world example: restaurant billing 
+price = float(input("Enter price: "))
+quantity = int(input("Enter quantity: "))
+
+total = price * quantity
+
+print("Total:", total)
