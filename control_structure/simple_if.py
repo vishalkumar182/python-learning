b5c509d446e0967fe208d39983130817d5fdd089
vishalkumar182 <vishal8049kumar@gmail.com>
2026-08-27 15:13:52 +0530

    simple if in python

---
+++ control_structure/simple_if.py
@@ -0,0 +1,14 @@
+# syntax 
+# if condition: 
+#     //code
+age =int(input("Enter your age: "))  # takes input from the user and assigns it to the variable 'age'
+if age >= 18:  # checks if the age is greater than or equal to 18
+ print("You are eligible to vote.")  # executes this block if the condition is true
+
+ # condition is false, nothing will be printed 
+
+ # Mu;ltiple statements can be executed in the if block by using indentation. For example:
+if age >= 18:  # checks if the age is greater than or equal to 18
+    print("You are eligible to vote.")  # executes this block if the condition is true
+    print("You can also apply for a driving license.")  # executes this block if the condition is true
+    
