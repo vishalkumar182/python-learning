40727c4a8c84164c73672e28dcede89b24cba2fa
Vishal Kumar <vishal8049kumar@gmail.com>
2026-08-28 21:52:16 +0530

    Add tuples example with fruit and number lists

---
+++ tuples.py
@@ -0,0 +1,10 @@
+numbers = (10, 20, 30, 40)
+my_list = [10, 20, 30]
+my_tuple = (10, 20, 30)fruits = ("apple", "banana", "mango")
+print(fruits)
+fruits = ("apple", "banana", "mango")
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[-1])
