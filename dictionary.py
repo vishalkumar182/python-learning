5c14189fae250ee3126cce53c502de71209be31e
vishalkumar182 <vishal8049kumar@gmail.com>
2026-09-12 13:21:54 +0530

    dictionary in python

--- dictionary.py
+++ dictionary.py
@@ -45,4 +45,16 @@ print(instagram_user["username"])
 
 # Instead of remembering positions like 0, 1, 2, we use meaningful keys.
 
+# 3. Creating a Dictionary
+# most common way to create a dictionary is by using curly braces {} and separating keys and values with a colon :.
+example = {
+    "key1": "value1",
+    "key2": "value2"
+}
 
+# empty dictionary can be created using empty curly braces {} or the dict() constructor.
+empty_dict1 = {}
+empty_dict2 = dict()
+
+# later we can add key-value pairs to the dictionary using assignment.
+empty_dict1["new_key"] = "new_value"
